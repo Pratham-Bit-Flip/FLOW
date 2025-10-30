baa1ad67d8023e8457524801c228db6c9095223d
Desai Prathamesh <spratham1526@gmail.com>
2025-10-30 07:40:26 +0530

    Update reg_file_tb.v

--- RISC-V/tb/reg_file_tb.v
+++ RISC-V/tb/reg_file_tb.v
@@ -29,37 +29,26 @@ module reg_file_tb;
         
         $monitor("clk=%b | wr_en=%b | rs1=%0d->%0d | rs2=%0d->%0d | rd=%0d | wr_data=%0d",
          clk, wr_en, rs1, rd_data1, rs2, rd_data2, rd, wr_data);
-
         // initialization
         clk = 0; wr_en = 0;rs1 = 0; rs2 = 0; rd = 0;wr_data = 0;
         #10;
-
         // write into reg1
         wr_en = 1; rd = 5'd1; wr_data = 32'd15;
         #10; 
-        
         wr_en = 0;
         #10;
-
         // read from reg1 and reg0
         rs1 = 5'd1; rs2 = 5'd0;
         #10;
-        
-
         // write into reg5
         wr_en = 1; rd = 5'd5; wr_data = 32'd100;
         #10; 
-        
         wr_en = 0;
         #10;
-
         // read reg5 and reg1
         rs1 = 5'd5; rs2 = 5'd1;
         #10;
-
         $finish;
-        $display("Time=%0t | rs1=%0d -> %0d | rs2=%0d -> %0d", 
-                 $time, rs1, rd_data1, rs2, rd_data2);
 
     end
 endmodule
